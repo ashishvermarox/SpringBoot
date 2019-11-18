9695ee73ea2a38e548942434c2a2e5ada59ef048
ashishvermarox <ashish_verma@live.com>
2019-11-17 23:47:32 -0500

    Removed unused import

--- shopping/src/main/java/shopping/model/Person.java
+++ shopping/src/main/java/shopping/model/Person.java
@@ -8,7 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
